60750e160cf78effa0210e3554bfdffc70bf3243
JoseMiguelGonzalez098 <60407733+JoseMiguelGonzalez098@users.noreply.github.com>
2021-04-08 13:39:45 -0600

    Add files via upload
    Se arreglo el main y las funciones ya estan habilitadas.

--- main.java
+++ main.java
@@ -113,192 +113,201 @@ class main{
         Scanner scan = new Scanner(System.in);//hola mi amor 
         
         int op;
-            
-        System.out.println("\n\n-----------.M.E.N.U.-----------");
-        System.out.println("1. HashMap");//NO SE SI JALA
-        System.out.println("2. TreeMap");//NO SE SI JALA
-        System.out.println("3. LinkedHashMap\n\n");//NO SE SI JALA
-        op = scan.nextInt();
+        try{
 
-        int op1 = -1;
-        int opcion1= -1;
+            System.out.println("\n\n-----------.M.E.N.U.-----------");
+            System.out.println("1. HashMap");//NO SE SI JALA
+            System.out.println("2. TreeMap");//NO SE SI JALA
+            System.out.println("3. LinkedHashMap\n\n");//NO SE SI JALA
+            op = scan.nextInt();
 
-        //SE CREA COLECCION---------------------------------------------------------------------------------
+            int op1 = -1;
+            int opcion1= -1;
 
-
-        ArrayList<String> Muebles2 = new ArrayList<String>();
-        ArrayList<String> Sillones2 = new ArrayList<String>();
-        ArrayList<String> Bebidas2 = new ArrayList<String>();
-        ArrayList<String> Condimentos2 = new ArrayList<String>();
-        ArrayList<String> Carne2 = new ArrayList<String>();
-        ArrayList<String> Frutas2 = new ArrayList<String>();
-        ArrayList<String> Lacteos2 = new ArrayList<String>();
-
-        AbstractoHash coleccion = trabajar(op);
-        coleccion.crear("Mueble", Muebles2);
-        coleccion.crear("Sillones", Sillones2);
-        coleccion.crear("Bebidas", Bebidas2);
-        coleccion.crear("Condimento", Condimentos2);
-        coleccion.crear("Carnes", Carne2);
-        coleccion.crear("Frutas", Frutas2);
-        coleccion.crear("Lacteos", Lacteos2);
+            //SE CREA COLECCION---------------------------------------------------------------------------------
 
 
-        //Aqui empieza nuestro menu y opciones para pedir que tipo de Map quiere usar.
-        //**************************************************************************************************** */
-        while (opcion1 != 5) {
-            
-            
-            //PARA HASHMAP
+            ArrayList<String> Muebles2 = new ArrayList<String>();
+            ArrayList<String> Sillones2 = new ArrayList<String>();
+            ArrayList<String> Bebidas2 = new ArrayList<String>();
+            ArrayList<String> Condimentos2 = new ArrayList<String>();
+            ArrayList<String> Carne2 = new ArrayList<String>();
+            ArrayList<String> Frutas2 = new ArrayList<String>();
+            ArrayList<String> Lacteos2 = new ArrayList<String>();
 
-            System.out.println("1. Ingresa un producto a la coleccion");//NO SE SI JALA
-            System.out.println("2. Categoria de producto");//NO SE SI JALA
-            System.out.println("3. Informacion de coleccion");//NO SE SI JALA
-            System.out.println("4. Informacion del inventario");//NO SE SI JALA
-            System.out.println("5. Salir\n\n");//NO SE SI JALA
+            AbstractoHash coleccion = trabajar(op);
+            coleccion.crear("Mueble", Muebles2);
+            coleccion.crear("Sillones", Sillones2);
+            coleccion.crear("Bebidas", Bebidas2);
+            coleccion.crear("Condimento", Condimentos2);
+            coleccion.crear("Carnes", Carne2);
+            coleccion.crear("Frutas", Frutas2);
+            coleccion.crear("Lacteos", Lacteos2);
 
-            String pedir = scan.next();
 
-            try {
+            //Aqui empieza nuestro menu y opciones para pedir que tipo de Map quiere usar.
+            //**************************************************************************************************** */
+            while (opcion1 != 5) {
+                
+                
+                //PARA HASHMAP
 
-                op1 = Integer.parseInt(pedir);
-                opcion1 = op1;
+                System.out.println("1. Ingresa un producto a la coleccion");//NO SE SI JALA
+                System.out.println("2. Categoria de producto");//NO SE SI JALA
+                System.out.println("3. Informacion de coleccion");//NO SE SI JALA
+                System.out.println("4. Informacion del inventario");//NO SE SI JALA
+                System.out.println("5. Salir\n\n");//NO SE SI JALA
 
-                if (op1 == 1) {//PARA INGRESAR UN PRODUCTO
-            
-                    System.out.println("Ingrese la categoria del producto que quiere agregar.");
-                    String categoria = scan.next();
+                String pedir = scan.next();
 
-                    try{
+                try {
 
-                        System.out.println("Ingrese el nuevo producto.");
-                        scan.nextLine();
-                        String NuevoProducto =scan.nextLine();
-                        String respuesta = inventario.buscar(NuevoProducto);
-                        
-                        if(respuesta.equals("")){
+                    op1 = Integer.parseInt(pedir);
+                    opcion1 = op1;
 
-                            System.out.println("El producto no existe");
+                    if (op1 == 1) {//PARA INGRESAR UN PRODUCTO
+                
+                        System.out.println("Ingrese la categoria del producto que quiere agregar.");
+                        String categoria = scan.next();
+
+                        try{
+
+                            System.out.println("Ingrese el nuevo producto.");
+                            scan.nextLine();
+                            String NuevoProducto =scan.nextLine();
+                            String respuesta = inventario.buscar(NuevoProducto);
                             
-                        } else{
-                            
-                            coleccion.agregar(categoria, NuevoProducto);
-                            System.out.println("Se ha agregado un nuevo producto a la coleccion\n");
-                        
-                        }
+                            if(respuesta.equals("")){
 
-                    }catch(Exception E){
-
-                        System.out.println("La categoria no existe. ");
-
-                    }
-
-
-
-                }else if(op1 == 2){//PARA MOSTRAR CATEGORIA DE PRODCUTO_______________________--//si deberia de funcionar
-                    
-                    System.out.println("Ingrese el nombre del producto del que quiere saber la categoria.");
-                    String categoria = scan.next();
-
-                    if(!inventario.GivoKey(categoria).equals(null)){
-
-                        System.out.println("La categoria del producto " + inventario.GivoKey(categoria)+ "\n");
-
-                    }else{
-
-                        System.out.println("La categoria no existe :(");
-
-                    }
-
-                } else if(op1 == 3){//PARA PEDIR INFO DE LA COLECCION 
-
-                    int op2 = -1;
-                    int opcion2 = -1;
-                    while (opcion2 != 3) {
-            
-                        System.out.println("1. Mostrar articulos en la coleccion" );//NO SE SI JALA
-                        System.out.println("2. Mostrar articulos en la coleccion por tipo" );//NO SE SI JALA
-                        System.out.println("3. Salir\n\n");//NO SE SI JALA
-                        try {
-                            op2 = scan.nextInt();
-                            opcion2 = op;
-            
-                            if (op2 == 1) {
-            
-                                coleccion.recorrer();
-            
-                            } else if(op2 == 2){
-            
-                                coleccion.recorrerTipo();
-            
-                            } else if(op2 == 3){
+                                System.out.println("El producto no existe");
                                 
-                                opcion2 = 3;
-
                             } else{
-            
-                                System.out.println("La opcion que has ingresado no es valida");
-                            }
-                            
-                        }catch (Exception e){
-                            //TODO: handle exception
-                            System.out.println("Debes ingresar un valor numerico como opcion");
-                        }
-                    }
-                    
-                } else if(op1 == 4){//MOSTRAR INFO DE INVENTARIO
-
-                    int op2 = -1;
-                    int opcion2 = -1;
-                    while (opcion2 != 3) {
-            
-                        System.out.println("1. Mostrar articulos en el inventario y su cantidad" );//SI JALA
-                        System.out.println("2. Mostrar articulos en el inventario por tipo" );//SI JALA
-                        System.out.println("3. Salir\n\n");//NO SE SI JALA
-                        try {
-                            op2 = scan.nextInt();
-                            opcion2 = op;
-            
-                            if (op2 == 1) {
-            
-                                inventario.recorrer();
-            
-                            } else if(op2 == 2){
-            
-                                inventario.recorrerTipo();
-            
-                            } else if(op2 == 3){
                                 
-                                opcion2 = 3;
-
-                            } else{
-            
-                                System.out.println("La opcion que has ingresado no es valida");
-                            }
+                                coleccion.agregar(categoria, NuevoProducto);
+                                System.out.println("Se ha agregado un nuevo producto a la coleccion\n");
                             
-                        }catch (Exception e){
-                            //TODO: handle exception
-                            System.out.println("Debes ingresar un valor numerico como opcion");
+                            }
+
+                        }catch(Exception E){
+
+                            System.out.println("La categoria no existe. ");
+
                         }
+
+
+
+                    }else if(op1 == 2){//PARA MOSTRAR CATEGORIA DE PRODCUTO_______________________--//si deberia de funcionar
+                        
+                        System.out.println("Ingrese el nombre del producto del que quiere saber la categoria.");
+                        String categoria = scan.next();
+
+                        if(!inventario.GivoKey(categoria).equals(null)){
+
+                            System.out.println("La categoria del producto " + inventario.GivoKey(categoria)+ "\n");
+
+                        }else{
+
+                            System.out.println("La categoria no existe :(");
+
+                        }
+
+                    } else if(op1 == 3){//PARA PEDIR INFO DE LA COLECCION 
+
+                        int op2 = -1;
+                        int opcion2 = -1;
+                        while (opcion2 != 3) {
+                
+                            System.out.println("1. Mostrar articulos en la coleccion" );//NO SE SI JALA
+                            System.out.println("2. Mostrar articulos en la coleccion por tipo" );//NO SE SI JALA
+                            System.out.println("3. Salir\n\n");//NO SE SI JALA
+                            try {
+                                op2 = scan.nextInt();
+                                opcion2 = op;
+                
+                                if (op2 == 1) {
+                
+                                    coleccion.recorrer();
+                
+                                } else if(op2 == 2){
+                
+                                    coleccion.recorrerTipo();
+                
+                                } else if(op2 == 3){
+                                    
+                                    opcion2 = 3;
+
+                                } else{
+                
+                                    System.out.println("La opcion que has ingresado no es valida");
+                                }
+                                
+                            }catch (Exception e){
+                                //TODO: handle exception
+                                System.out.println("Debes ingresar un valor numerico como opcion");
+                            }
+                        }
+                        
+                    } else if(op1 == 4){//MOSTRAR INFO DE INVENTARIO
+
+                        int op2 = -1;
+                        int opcion2 = -1;
+                        while (opcion2 != 3) {
+                
+                            System.out.println("1. Mostrar articulos en el inventario y su cantidad" );//SI JALA
+                            System.out.println("2. Mostrar articulos en el inventario por tipo" );//SI JALA
+                            System.out.println("3. Salir\n\n");//NO SE SI JALA
+                            try {
+                                op2 = scan.nextInt();
+                                opcion2 = op;
+                
+                                if (op2 == 1) {
+                
+                                    inventario.recorrer();
+                
+                                } else if(op2 == 2){
+                
+                                    inventario.recorrerTipo();
+                
+                                } else if(op2 == 3){
+                                    
+                                    opcion2 = 3;
+
+                                } else{
+                
+                                    System.out.println("La opcion que has ingresado no es valida");
+                                }
+                                
+                            }catch (Exception e){
+                                //TODO: handle exception
+                                System.out.println("Debes ingresar un valor numerico como opcion");
+                            }
+                        }
+
+                    } else if(op1 == 5){
+
+                        System.out.println("Adios UwU");
+                        opcion1 = 5;
+                        
+                    } else{
+
+                        System.out.println("La opcion que has ingresado no es valida");
                     }
-
-                } else if(op1 == 5){
-
-                    System.out.println("Adios UwU");
-                    opcion1 = 5;
                     
-                } else{
-
-                    System.out.println("La opcion que has ingresado no es valida");
+                }catch (Exception e){
+                    //TODO: handle exception
+                    System.out.println("Debes ingresar un valor numerico como opcion");
+                    
                 }
                 
-            }catch (Exception e){
-                //TODO: handle exception
-                System.out.println("Debes ingresar un valor numerico como opcion");
-                
             }
+
+        }catch(Exception E){
+
+            System.out.println("Debes ingresar un valor numerico como opcion");
             
         }
+            
+        
     }
     
 }
